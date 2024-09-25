78cf104d27bc64af99a10d0e6b346810e771d1bf
Marcin Zubilewicz <mzubilewicz4@gmail.com>
2024-09-25 03:43:40 +0200

    Bind new tree nav commands, add options support

--- lua/nvim-treeclimber.lua
+++ lua/nvim-treeclimber.lua
@@ -11,7 +11,6 @@ function M.setup_keymaps()
 	vim.keymap.set("n", "<leader>k", tc.show_control_flow, {})
 
 	vim.keymap.set({ "x", "o" }, "i.", tc.select_current_node, { desc = "select current node" })
-
 	vim.keymap.set({ "x", "o" }, "a.", tc.select_expand, { desc = "select parent node" })
 
 	vim.keymap.set(
@@ -24,13 +23,14 @@ function M.setup_keymaps()
 	vim.keymap.set(
 		{ "n", "x", "o" },
 		"<M-b>",
-		tc.select_backward,
+		tc.select_current_node,
 		{ desc = "select and move to the begining of the node, or the beginning of the next node" }
 	)
 
 	vim.keymap.set({ "n", "x", "o" }, "<M-[>", tc.select_siblings_backward, {})
-
 	vim.keymap.set({ "n", "x", "o" }, "<M-]>", tc.select_siblings_forward, {})
+	vim.keymap.set({ "n", "x", "o" }, "<M-{>", tc.select_siblings_visual_backward, {})
+	vim.keymap.set({ "n", "x", "o" }, "<M-}>", tc.select_siblings_visual_forward, {})
 
 	vim.keymap.set(
 		{ "n", "x", "o" },
@@ -39,17 +39,14 @@ function M.setup_keymaps()
 		{ desc = "select the top level node from the current position" }
 	)
 
-	vim.keymap.set({ "n", "x", "o" }, "<M-h>", tc.select_backward, { desc = "select previous node" })
-
-	vim.keymap.set({ "n", "x", "o" }, "<M-j>", tc.select_shrink, { desc = "select child node" })
-
-	vim.keymap.set({ "n", "x", "o" }, "<M-k>", tc.select_expand, { desc = "select parent node" })
-
 	vim.keymap.set({ "n", "x", "o" }, "<M-l>", tc.select_forward, { desc = "select the next node" })
-
-	vim.keymap.set({ "n", "x", "o" }, "<M-L>", tc.select_grow_forward, { desc = "Add the next node to the selection" })
-
-	vim.keymap.set({ "n", "x", "o" }, "<M-H>", tc.select_grow_backward, { desc = "Add the next node to the selection" })
+	vim.keymap.set({ "n", "x", "o" }, "<M-h>", tc.select_backward, { desc = "select previous node" })
+	vim.keymap.set({ "n", "x", "o" }, "<M-j>", tc.select_shrink, { desc = "select child node" })
+	vim.keymap.set({ "n", "x", "o" }, "<M-k>", tc.select_expand, { desc = "select parent node" })
+	vim.keymap.set({ "n", "x", "o" }, "<M-J>", tc.select_grow_forward, { desc = "Add the next node to the selection" })
+	vim.keymap.set({ "n", "x", "o" }, "<M-K>", tc.select_grow_backward, { desc = "Add the previous node to the selection" })
+	vim.keymap.set({ "n", "x", "o" }, "<M-L>", tc.select_visual_forward, { desc = "Enlarge the selection to the next node" })
+	vim.keymap.set({ "n", "x", "o" }, "<M-H>", tc.select_visual_backward, { desc = "Enlarge the selection to the previous node" })
 end
 
 function M.setup_user_commands()
@@ -68,7 +65,14 @@ function M.setup_user_commands()
 	})
 end
 
-function M.setup_highlight()
+function M.setup_highlight(opts)
+	local opts = opts or {}
+	local alpha		 = opts['alpha'] or 50
+	local alpha_sb = opts['alpha-sibling-bd']		or alpha
+	local alpha_s  = opts['alpha-sibling']			or alpha
+	local alpha_p  = opts['alpha-parent']				or alpha
+	local alpha_ps = opts['alpha-parent-start'] or alpha
+
 	-- Must run after colorscheme or TermOpen to ensure that terminal_colors are available
 	local hi = require("nvim-treeclimber.hi")
 
@@ -81,28 +85,28 @@ function M.setup_highlight()
 	local visual = hi.HSLUVHighlight:new(Visual)
 
 	vim.api.nvim_set_hl(0, "TreeClimberHighlight", { background = visual.bg.hex })
-	vim.api.nvim_set_hl(0, "TreeClimberSiblingBoundary", { background = visual.bg.mix(normal.bg, 50).hex })
-	vim.api.nvim_set_hl(0, "TreeClimberSibling", { background = visual.bg.mix(normal.bg, 50).hex })
-	vim.api.nvim_set_hl(0, "TreeClimberParent", { background = visual.bg.mix(normal.bg, 50).hex })
-	vim.api.nvim_set_hl(0, "TreeClimberParentStart", { background = visual.bg.mix(normal.bg, 50).hex })
+	vim.api.nvim_set_hl(0, "TreeClimberSiblingBoundary", { background = visual.bg.mix(normal.bg, 100 - alpha_sb).hex })
+	vim.api.nvim_set_hl(0, "TreeClimberSibling", { background = visual.bg.mix(normal.bg, 100 - alpha_s).hex })
+	vim.api.nvim_set_hl(0, "TreeClimberParent", { background = visual.bg.mix(normal.bg, 100 - alpha_p).hex })
+	vim.api.nvim_set_hl(0, "TreeClimberParentStart", { background = visual.bg.mix(normal.bg, 100 - alpha_ps).hex })
 end
 
-function M.setup_augroups()
+function M.setup_augroups(opts)
 	local group = vim.api.nvim_create_augroup("nvim-treeclimber-colorscheme", { clear = true })
 
 	vim.api.nvim_create_autocmd({ "Colorscheme" }, {
 		group = group,
 		pattern = "*",
 		callback = function()
-			M.setup_highlight()
+			M.setup_highlight(opts)
 		end,
 	})
 end
 
-function M.setup()
+function M.setup(opts)
 	M.setup_keymaps()
 	M.setup_user_commands()
-	M.setup_augroups()
+	M.setup_augroups(opts)
 end
 
 return M
